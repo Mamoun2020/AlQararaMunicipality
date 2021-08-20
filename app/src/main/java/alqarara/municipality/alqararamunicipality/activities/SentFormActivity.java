f9acdf7ea0d1b4fb44bc5414a34ea858a989e5d2
Mamoun2020 <mmun2019@gmail.com>
2021-08-20 14:20:19 +0300

    form

--- app/src/main/java/alqarara/municipality/alqararamunicipality/activities/SentFormActivity.java
+++ app/src/main/java/alqarara/municipality/alqararamunicipality/activities/SentFormActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -53,6 +54,183 @@ public class SentFormActivity extends AppCompatActivity {
         inflate();
         configureToolbar();
         Firebase.setAndroidContext(this);
+
+        sp_area.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (sp_area.getSelectedItem().equals("منطقة 86")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("مسجد التوحيد");
+                    arrayList.add("مسجد الرضوان");
+                    arrayList.add("مسجد بلال بن رباح");
+                    arrayList.add("مدرسة المعري");
+                    arrayList.add("روضة الأشبال النموذجية");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+                else if(sp_area.getSelectedItem().equals("السريج")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("مسجد الرحمن");
+                    arrayList.add("نادي القرارة الرياضي");
+                    arrayList.add("مدرسة كامل الأغا");
+                    arrayList.add("مدرسة النور");
+                    arrayList.add("روضة المروة");
+                    arrayList.add("مسجد عمر بن الخطاب");
+                    arrayList.add("مسجد أبو بكر الصديق");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+                else if(sp_area.getSelectedItem().equals("أل عبد الغفور و فياض")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("عيادة الزنة");
+                    arrayList.add("مدرسة محمد بن صالح ابن عثمين");
+                    arrayList.add("مسجد أسامة بن زيد");
+                    arrayList.add("مجلس العلمي للدعوة السلفية");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+                else if(sp_area.getSelectedItem().equals("منطقة العبادلة")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("مسجد كامل الأسطل");
+                    arrayList.add("روضة العلماء الصغار");
+                    arrayList.add("مسجد الهدى");
+                    arrayList.add("مسجد المحسنين");
+                    arrayList.add("مسجد الاستقامة");
+                    arrayList.add("روضة الكوثر");
+                    arrayList.add("جمعية الانسان التنموية");
+                    arrayList.add("مدرسة القرارة");
+                    arrayList.add("مسجد الصحابة");
+                    arrayList.add("جمعية دار الكتاب و السنة");
+                    arrayList.add("مسجد خالد بن الوليد");
+                    arrayList.add("مسجد السلام");
+                    arrayList.add("عيادة القرارة");
+                    arrayList.add("مصلى عمر بن الخطاب");
+                    arrayList.add("مدرسة عيلبون");
+                    arrayList.add("روضة القدس النموذجية");
+                    arrayList.add("عيادة مسقط");
+                    arrayList.add("مدرسة مسقط");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+                else if(sp_area.getSelectedItem().equals("منطقة الغربية")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("مدرسة الوكالة الإبتدائية");
+                    arrayList.add("مركز شرطة القرارة");
+                    arrayList.add("مدرسة الوكالة الإعدادية");
+                    arrayList.add("المطاحن الفلسطينية");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+                else{
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_near.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sp_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(sp_category.getSelectedItem().equals("قسم المياه والصرف الصحي")) {
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("وصلات غير شرعية");
+                    arrayList.add("اهدار مياه");
+                    arrayList.add("استفسار مياه");
+                    arrayList.add("انفجار خط مياه");
+                    arrayList.add("ملوحة مياه");
+                    arrayList.add("سرقة مياه");
+                    arrayList.add("انقطاع مياه");
+                    arrayList.add("منهل مفتوح");
+                    arrayList.add("توصيل خطوط الصرف الصحي");
+                    arrayList.add("مصافي مياه أمطار");
+                    arrayList.add("مشاريع صرف صحي");
+                    arrayList.add("طفح صرف صحي");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_item, arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+                else if(sp_category.getSelectedItem().equals("قسم التنظيم والتخطيط")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("ترميم مباني");
+                    arrayList.add("حرفة بدون ترخيص");
+                    arrayList.add("اشغال رصيف");
+                    arrayList.add("بناء بدون ترخيص");
+                    arrayList.add("رسوم تنظيم");
+                    arrayList.add("بناء لإيل للسقوط");
+                    arrayList.add("التسجيل لحصة أسمنت");
+                    arrayList.add("البسطات العشوائية");
+                    arrayList.add("تعديات");
+                    arrayList.add("ازعاج مكبرات صوت");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item,arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+                else if(sp_category.getSelectedItem().equals("قسم المالية")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("فواتير خدمات");
+                    arrayList.add("إشكالات مالية");
+                    arrayList.add("فواتير عامة");
+                    arrayList.add("تركيب يافظة");
+                    arrayList.add("قراءة عداد مياه");
+                    arrayList.add("مواقف البلدية للسيارات");
+                    arrayList.add("استفسار عن اليافطات");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item,arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+                else if(sp_category.getSelectedItem().equals("الشؤون الإدارية")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("طلب تطوع");
+                    arrayList.add("طلب توظيف");
+                    arrayList.add("مخالفات موظفين");
+                    arrayList.add("استفسار عن وظيفة");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item,arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+                else if(sp_category.getSelectedItem().equals("قسم الصيانة")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("عمل مطبات");
+                    arrayList.add("خلع بلاط");
+                    arrayList.add("رصف شارع");
+                    arrayList.add("حفر طرق");
+                    arrayList.add("استفسار عن مشاريع طرق");
+                    arrayList.add("حفرة شارع");
+                    arrayList.add("انارة شوارع");
+                    arrayList.add("اصلاح فانوس");
+                    arrayList.add("تركيب فانوس جديد");
+                    arrayList.add("كلاب ضالة");
+                    arrayList.add("موالدات كهربائية");
+                    arrayList.add("طلب حاويات");
+                    arrayList.add("عما نظافة تصنيع");
+                    arrayList.add("انتشار بعوض");
+                    arrayList.add("انتشار قوارص");
+                    arrayList.add("قطع أشجار");
+                    arrayList.add("تشجير");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item,arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+                else if(sp_category.getSelectedItem().equals("شكوى عامة")){
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("طلب الغاء معاملة");
+                    arrayList.add("طلب افادة");
+                    arrayList.add("مقترحات تطويرية");
+                    arrayList.add("طلب تعويضات");
+                    arrayList.add("استفسار عن قوانين و أنظمة");
+                    arrayList.add("استفسار عن حجز قاعة");
+                    arrayList.add("استفسار عن ايجار مكان");
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item,arrayList);
+                    sp_type.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         btn_sent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,21 +255,80 @@ public class SentFormActivity extends AppCompatActivity {
                         Firebase reference = new Firebase("https://alqararamunicipality-b276d-default-rtdb.firebaseio.com/forms");
                         int random = new Random().nextInt(9000);
                         if(s.equals("null")) {
-                            reference.child(String.valueOf(random)).child("id").setValue(id);
-                            reference.child(String.valueOf(random)).child("phone").setValue(phone);
-                            reference.child(String.valueOf(random)).child("title").setValue(title);
-                            reference.child(String.valueOf(random)).child("milepost").setValue(near);
-                            reference.child(String.valueOf(random)).child("area").setValue(area);
-                            reference.child(String.valueOf(random)).child("description").setValue(description);
-                            reference.child(String.valueOf(random)).child("category").setValue(category);
-                            reference.child(String.valueOf(random)).child("type").setValue(type);
-                            reference.child(String.valueOf(random)).child("text").setValue(text);
-                            chat();
+                            if( TextUtils.isEmpty(id))
+                            {
+                                et_id.setError( "id is required!" );
+                            }
+                            else if( TextUtils.isEmpty(phone)){
+                                et_phone.setError( "phone is required!" );
+                            }
+                            else if(TextUtils.isEmpty(title)){
+                                et_title.setError( "title is required!" );
+                            }
+                            else if(TextUtils.isEmpty(near)){
+                                ((TextView)sp_near.getSelectedView()).setError("near is required!");
+                            }
+                            else if(TextUtils.isEmpty(area)){
+                                ((TextView)sp_area.getSelectedView()).setError("area is required!");
+                            }
+                            else if(TextUtils.isEmpty(description)){
+                                et_description.setError( "description is required!" );
+                            }
+                            else if(TextUtils.isEmpty(category) ){
+                                ((TextView)sp_category.getSelectedView()).setError("category is required!");
+                            }
+                            else if(TextUtils.isEmpty(type) ){
+                                ((TextView)sp_type.getSelectedView()).setError("type is required!");
+                            }
+                            else if(TextUtils.isEmpty(text)){
+                                et_text.setError( "text is required!" );
+                            }
+                            else{
+                                reference.child(String.valueOf(random)).child("id").setValue(id);
+                                reference.child(String.valueOf(random)).child("phone").setValue(phone);
+                                reference.child(String.valueOf(random)).child("title").setValue(title);
+                                reference.child(String.valueOf(random)).child("milepost").setValue(near);
+                                reference.child(String.valueOf(random)).child("area").setValue(area);
+                                reference.child(String.valueOf(random)).child("description").setValue(description);
+                                reference.child(String.valueOf(random)).child("category").setValue(category);
+                                reference.child(String.valueOf(random)).child("type").setValue(type);
+                                reference.child(String.valueOf(random)).child("text").setValue(text);
+                                chat();
+                            }
 //                            startActivity(new Intent(SentFormActivity.this, ChatActivity.class));
                         }
                         else {
                             try {
                                 JSONObject obj = new JSONObject(s);
+                                if( TextUtils.isEmpty(id))
+                                {
+                                    et_id.setError( "id is required!" );
+                                }
+                                else if( TextUtils.isEmpty(phone)){
+                                    et_phone.setError( "phone is required!" );
+                                }
+                                else if(TextUtils.isEmpty(title)){
+                                    et_title.setError( "title is required!" );
+                                }
+                                else if(TextUtils.isEmpty(near)){
+                                    ((TextView)sp_near.getSelectedView()).setError("near is required!");
+                                }
+                                else if(TextUtils.isEmpty(area)){
+                                    ((TextView)sp_area.getSelectedView()).setError("area is required!");
+                                }
+                                else if(TextUtils.isEmpty(description)){
+                                    et_description.setError( "description is required!" );
+                                }
+                                else if(TextUtils.isEmpty(category) ){
+                                    ((TextView)sp_category.getSelectedView()).setError("category is required!");
+                                }
+                                else if(TextUtils.isEmpty(type) ){
+                                    ((TextView)sp_type.getSelectedView()).setError("type is required!");
+                                }
+                                else if(TextUtils.isEmpty(text)){
+                                    et_text.setError( "text is required!" );
+                                }
+                                else{
                                     reference.child(String.valueOf(random)).child("id").setValue(id);
                                     reference.child(String.valueOf(random)).child("phone").setValue(phone);
                                     reference.child(String.valueOf(random)).child("title").setValue(title);
@@ -101,7 +338,8 @@ public class SentFormActivity extends AppCompatActivity {
                                     reference.child(String.valueOf(random)).child("category").setValue(category);
                                     reference.child(String.valueOf(random)).child("type").setValue(type);
                                     reference.child(String.valueOf(random)).child("text").setValue(text);
-                                chat();
+                                    chat();
+                                }
                             } catch (JSONException e) {
                                  e.printStackTrace();
                             }
